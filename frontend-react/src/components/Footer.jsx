f2f4dd76e68c99b067a33858897b52cb7464f254
Deeptidash025 <deeptidash20025@gmail.com>
2025-07-22 18:03:30 +0530

    Homepage React

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className="footer py-3 my-3">
+        <hr className='border-buttom' />
+        <p className="text-light text-center">&copy; 2025-Built with passion</p>
+    </footer>
+    
+    </>
+  )
+}
+
+export default Footer
